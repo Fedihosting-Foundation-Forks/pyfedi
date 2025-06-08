8823b47ae0d56a00408fdb4aa609f45c5284795e
MrKaplan <169855803+MrKaplan-lw@users.noreply.github.com>
2025-06-09 01:37:30 +0200

    Add datadog tracing

--- entrypoint_celery.sh
+++ entrypoint_celery.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-celery -A celery_worker_docker.celery worker --autoscale=5,1 --queues=celery,background,send
+ddtrace-run celery -A celery_worker_docker.celery worker --autoscale=5,1 --queues=celery,background,send
 

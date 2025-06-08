b94fe252bc66ad675bf3bafcfa6ecb8376ce4ffc
MrKaplan <169855803+MrKaplan-lw@users.noreply.github.com>
2025-06-09 01:37:30 +0200

    Add datadog tracing

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,15 @@ set -e
 export FLASK_APP=pyfedi.py
 
 echo "Running database migrations..."
-flask db upgrade
+ddtrace-run flask db upgrade
 
-flask populate_community_search
+ddtrace-run flask populate_community_search
 
 if [ "${FLASK_DEBUG:-}" = "1" ] && [ "${FLASK_ENV:-}" = "development" ]; then
   export FLASK_RUN_EXTRA_FILES=$(find app/templates app/static -type f | tr '\n' ':')
   echo "Starting flask development server..."
-  flask run -h 0.0.0.0 -p 5000
+  ddtrace-run flask run -h 0.0.0.0 -p 5000
 else
   echo "Starting Gunicorn..."
-  gunicorn --config gunicorn.conf.py --preload pyfedi:app
+  ddtrace-run gunicorn --config gunicorn.conf.py --preload pyfedi:app
 fi
